e95778e96f62e6d08ce7054c6364319f38786230
RotemAAA <rotem.guzman@gmail.com>
2020-03-15 19:43:17 +0200

    1. model

---
+++ app/src/main/java/rotem/guzman/todolist/Task.java
@@ -0,0 +1,18 @@
+package rotem.guzman.todolist;
+
+public class Task {
+    private String task;
+
+    public Task(String task) {
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+}
